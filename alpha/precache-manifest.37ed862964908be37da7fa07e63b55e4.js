8f41883ac89da5a3b5aa2ef76b78e5ee22a5db0c
Mathemagician78 <60906110+Mathemagician78@users.noreply.github.com>
2020-02-27 18:04:30 -0800

    Updates

---
+++ alpha/precache-manifest.37ed862964908be37da7fa07e63b55e4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1263cea2be926ca240630a28dc45fa19",
+    "url": "/coding-2-try-phaser-mathemagician78/alphaassets/Poof.png"
+  },
+  {
+    "revision": "90b7be82cd37eb98b01863f50b84ab62",
+    "url": "/coding-2-try-phaser-mathemagician78/alphaassets/bunny-hop-spritesheet.png"
+  },
+  {
+    "revision": "68915bb265ad4941b284d5b28494dc3d",
+    "url": "/coding-2-try-phaser-mathemagician78/alphaassets/carrot.png"
+  },
+  {
+    "revision": "82cc3c5479097d51f0818daf5f4be47b",
+    "url": "/coding-2-try-phaser-mathemagician78/alphaassets/citations.md"
+  },
+  {
+    "revision": "0ac26c5a70b28d696adba52a4074c533",
+    "url": "/coding-2-try-phaser-mathemagician78/alphaassets/environment_forest_alt1.png"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/coding-2-try-phaser-mathemagician78/alphaassets/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "/coding-2-try-phaser-mathemagician78/alphaassets/font.xml"
+  },
+  {
+    "revision": "8822c0c57b608c01898755ea857b4952",
+    "url": "/coding-2-try-phaser-mathemagician78/alphaassets/fox-SWEN-bright.png"
+  },
+  {
+    "revision": "5c5eb626426eb420f3e5f4ca68c4cfb0",
+    "url": "/coding-2-try-phaser-mathemagician78/alphaassets/grass.png"
+  },
+  {
+    "revision": "8c17c1515edf3260716c7812c638e3cb",
+    "url": "/coding-2-try-phaser-mathemagician78/alphaassets/pa2.png"
+  },
+  {
+    "revision": "4784118f47d91136d4cba54fee347994",
+    "url": "/coding-2-try-phaser-mathemagician78/alphaassets/rabbit.png"
+  },
+  {
+    "revision": "3a9c85fe5ad487a578aa67a6098fb6ac",
+    "url": "/coding-2-try-phaser-mathemagician78/alphaassets/screenshot_1.png"
+  },
+  {
+    "revision": "b567a7e434db20f3b83a",
+    "url": "/coding-2-try-phaser-mathemagician78/alphagen.main.caeb3096011342123517.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-mathemagician78/alphagen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "647a68331e9df191f10c65598c5ed984",
+    "url": "/coding-2-try-phaser-mathemagician78/alphaindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-mathemagician78/alphamanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-mathemagician78/alphasw.js"
+  }
+]);
---
+++ alpha/sw.js
@@ -0,0 +1,11 @@
+importScripts("/coding-2-try-phaser-mathemagician78/alphaprecache-manifest.37ed862964908be37da7fa07e63b55e4.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
