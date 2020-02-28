cc31d0063ca14adcc09bf4aa581564c9c7be275c
Mathemagician78 <60906110+Mathemagician78@users.noreply.github.com>
2020-02-27 18:10:30 -0800

    Updates

---
+++ beta/assets/citations.md
@@ -0,0 +1,15 @@
+*Please be sure to list the source of all assets you used here.*
+
+*At least a URL, but also be sure to include any extra information that the artist/author requests you include.*
+
+*https://opengameart.org/content/forest-background-art-2*
+
+*https://opengameart.org/content/easter-carrot-pick-up-item*
+
+*https://opengameart.org/content/bunny-sprite*
+
+*https://opengameart.org/content/poof-effect-spritesheet*
+
+*https://opengameart.org/content/fox-wolf-pack-rework*
+
+*https://opengameart.org/content/easter-egg*
---
+++ beta/sw.js
@@ -0,0 +1,11 @@
+importScripts("/coding-2-try-phaser-mathemagician78/betaprecache-manifest.57868239f24d1d9ef7f03f240d8505dd.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
