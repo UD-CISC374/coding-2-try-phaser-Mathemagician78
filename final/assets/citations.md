9ca57fc7d0d071eb95d5a1c637e09278d23153b6
Mathemagician78 <60906110+Mathemagician78@users.noreply.github.com>
2020-02-28 10:50:12 -0800

    Updates

---
+++ final/assets/citations.md
@@ -0,0 +1,27 @@
+*Please be sure to list the source of all assets you used here.*
+
+*At least a URL, but also be sure to include any extra information that the artist/author requests you include.*
+
+*https://opengameart.org/content/forest-background-art-2*
+
+*https://opengameart.org/content/easter-carrot-pick-up-item*
+
+*https://opengameart.org/content/bunny-sprite*
+
+*https://opengameart.org/content/poof-effect-spritesheet*
+
+*https://opengameart.org/content/fox-wolf-pack-rework*
+
+*https://opengameart.org/content/easter-egg*
+
+*https://opengameart.org/content/win-music-1*
+
+*https://opengameart.org/content/skull-death-effect-spritesheet*
+
+*https://opengameart.org/content/pleasing-bell-sound-effect*
+
+*https://opengameart.org/content/warp-sound-1*
+
+*https://opengameart.org/content/fantasy-logo-music-short-var-epic-orchestra-music*
+
+*https://opengameart.org/content/bubbles-pop*
---
+++ final/precache-manifest.a67af959e2c8d088cdcdace566c0d6fe.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1263cea2be926ca240630a28dc45fa19",
+    "url": "/coding-2-try-phaser-Mathemagician78/finalassets/Poof.png"
+  },
+  {
+    "revision": "9ed6cb8feb446c130e2cb805065b5b7a",
+    "url": "/coding-2-try-phaser-Mathemagician78/finalassets/Skull.png"
+  },
+  {
+    "revision": "90b7be82cd37eb98b01863f50b84ab62",
+    "url": "/coding-2-try-phaser-Mathemagician78/finalassets/bunny-hop-spritesheet.png"
+  },
+  {
+    "revision": "68915bb265ad4941b284d5b28494dc3d",
+    "url": "/coding-2-try-phaser-Mathemagician78/finalassets/carrot.png"
+  },
+  {
+    "revision": "41e07d8f6008374994164005f62e2f35",
+    "url": "/coding-2-try-phaser-Mathemagician78/finalassets/citations.md"
+  },
+  {
+    "revision": "0ac26c5a70b28d696adba52a4074c533",
+    "url": "/coding-2-try-phaser-Mathemagician78/finalassets/environment_forest_alt1.png"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/coding-2-try-phaser-Mathemagician78/finalassets/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "/coding-2-try-phaser-Mathemagician78/finalassets/font.xml"
+  },
+  {
+    "revision": "8822c0c57b608c01898755ea857b4952",
+    "url": "/coding-2-try-phaser-Mathemagician78/finalassets/fox-SWEN-bright.png"
+  },
+  {
+    "revision": "5c5eb626426eb420f3e5f4ca68c4cfb0",
+    "url": "/coding-2-try-phaser-Mathemagician78/finalassets/grass.png"
+  },
+  {
+    "revision": "1091ad6d4686178a215475256cd015ec",
+    "url": "/coding-2-try-phaser-Mathemagician78/finalassets/music.mp3"
+  },
+  {
+    "revision": "8c17c1515edf3260716c7812c638e3cb",
+    "url": "/coding-2-try-phaser-Mathemagician78/finalassets/pa2.png"
+  },
+  {
+    "revision": "e0d390c783f610d638e5c34e8005772c",
+    "url": "/coding-2-try-phaser-Mathemagician78/finalassets/pleasing-bell.wav"
+  },
+  {
+    "revision": "cae8bdd56df6425d46278ab33a748ee9",
+    "url": "/coding-2-try-phaser-Mathemagician78/finalassets/pop.ogg"
+  },
+  {
+    "revision": "4784118f47d91136d4cba54fee347994",
+    "url": "/coding-2-try-phaser-Mathemagician78/finalassets/rabbit.png"
+  },
+  {
+    "revision": "3a9c85fe5ad487a578aa67a6098fb6ac",
+    "url": "/coding-2-try-phaser-Mathemagician78/finalassets/screenshot_1.png"
+  },
+  {
+    "revision": "1286ed7b814c70e7844f2ac01f1a3a78",
+    "url": "/coding-2-try-phaser-Mathemagician78/finalassets/wave.wav"
+  },
+  {
+    "revision": "592f044bf13557d9f9aa6ec9e1beed83",
+    "url": "/coding-2-try-phaser-Mathemagician78/finalassets/win.wav"
+  },
+  {
+    "revision": "f33c53f7bbff87343b65",
+    "url": "/coding-2-try-phaser-Mathemagician78/finalgen.main.56c453ec1c5f04dc835e.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-Mathemagician78/finalgen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "4b6f1a9b78702f57034abad4fc87ddd1",
+    "url": "/coding-2-try-phaser-Mathemagician78/finalindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-Mathemagician78/finalmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-Mathemagician78/finalsw.js"
+  }
+]);
