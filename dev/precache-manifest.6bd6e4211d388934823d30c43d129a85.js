d5cf4aa994e2e6f0d309db85ad97fba7cbdc3adc
Mathemagician78 <60906110+Mathemagician78@users.noreply.github.com>
2020-02-27 17:39:55 -0800

    Updates

---
+++ dev/precache-manifest.6bd6e4211d388934823d30c43d129a85.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1263cea2be926ca240630a28dc45fa19",
+    "url": "/coding-2-try-phaser-mathemagician78/devassets/Poof.png"
+  },
+  {
+    "revision": "90b7be82cd37eb98b01863f50b84ab62",
+    "url": "/coding-2-try-phaser-mathemagician78/devassets/bunny-hop-spritesheet.png"
+  },
+  {
+    "revision": "68915bb265ad4941b284d5b28494dc3d",
+    "url": "/coding-2-try-phaser-mathemagician78/devassets/carrot.png"
+  },
+  {
+    "revision": "82cc3c5479097d51f0818daf5f4be47b",
+    "url": "/coding-2-try-phaser-mathemagician78/devassets/citations.md"
+  },
+  {
+    "revision": "0ac26c5a70b28d696adba52a4074c533",
+    "url": "/coding-2-try-phaser-mathemagician78/devassets/environment_forest_alt1.png"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/coding-2-try-phaser-mathemagician78/devassets/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "/coding-2-try-phaser-mathemagician78/devassets/font.xml"
+  },
+  {
+    "revision": "8822c0c57b608c01898755ea857b4952",
+    "url": "/coding-2-try-phaser-mathemagician78/devassets/fox-SWEN-bright.png"
+  },
+  {
+    "revision": "5c5eb626426eb420f3e5f4ca68c4cfb0",
+    "url": "/coding-2-try-phaser-mathemagician78/devassets/grass.png"
+  },
+  {
+    "revision": "8c17c1515edf3260716c7812c638e3cb",
+    "url": "/coding-2-try-phaser-mathemagician78/devassets/pa2.png"
+  },
+  {
+    "revision": "4784118f47d91136d4cba54fee347994",
+    "url": "/coding-2-try-phaser-mathemagician78/devassets/rabbit.png"
+  },
+  {
+    "revision": "3a9c85fe5ad487a578aa67a6098fb6ac",
+    "url": "/coding-2-try-phaser-mathemagician78/devassets/screenshot_1.png"
+  },
+  {
+    "revision": "901b847b021889998366",
+    "url": "/coding-2-try-phaser-mathemagician78/devgen.main.115e4c5f15d3c466eeda.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-mathemagician78/devgen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "ee52546191426cf43f407071b067ae27",
+    "url": "/coding-2-try-phaser-mathemagician78/devindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-mathemagician78/devmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-mathemagician78/devsw.js"
+  }
+]);
