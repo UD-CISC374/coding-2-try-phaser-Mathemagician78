ab3f35ae95ec00022e29238aca82c70c74699f08
Mathemagician78 <60906110+Mathemagician78@users.noreply.github.com>
2020-02-29 20:16:47 -0800

    Organized and commented code

--- src/scripts/objects/hurt.ts
+++ src/scripts/objects/hurt.ts
@@ -7,14 +7,11 @@ export default class hurt extends Phaser.GameObjects.Sprite {
         this.setScale(0.5);
         scene.add.existing(this);
         this.play("Perish", true);
-
-        
-
         
     }
 
+    //destroy if offscreen (probably unnecessary, but if I base another class on this, I'll remember to add it in for the new one now)
     update(){
-
         if(this.y<0){
             this.destroy();
         }
